0834d196f2eb6c6bd7e12920fcd9e9a167cc6917
JEILYMAR BRADY <jeilymarb@gmail.com>
2015-07-27 23:36:25 -0700

    added brady bunch logo and some ajax that is commented out

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,26 @@
+// $(document).ready(function() {
+//   var pics = [];
+//   $.ajax({
+//     url: 'https://api.imgur.com/3/album/b8p29#0.json',
+//     method: 'GET',
+//     headers: {
+//       'Authorization': 'Client-ID fc919a2e4fa23ab'
+//     }
+//   })
+//   .done(function(res) {
+//     pics = res.data.images;
+//     //Pushes picture links from pics to cards_array
+//     //var cards_array = [];
+//     function pullFromImgur() {
+//       for(var i = 0; i < 2; i++){
+//         for(var j = 0; j < pics.length; j++){
+//           cards_array.push(pics[j].link);
+//         }
+//         console.dir(cards_array);
+//       }
+//     }
+    //pullFromImgur();
+//game code
 var cards_array = ['A', 'A', 'B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
 var values = [];
 var ids = [];
@@ -47,16 +70,26 @@ function flipTile(tile, val){
           function flip(){
             var card_1 = document.getElementById(ids[0]);
             var card_2 = document.getElementById(ids[1]);
-            card_1.style.backgroundColor = 'lightblue'; //'url(tile_bg.jpg) no-repeat';
+            card_1.style.background = 'url(logo.png) no-repeat';
+            card_2.style.background = 'url(logo.png) no-repeat';
+            card_1.style.backgroundSize = 'cover';
+            card_2.style.backgroundSize = 'cover';
+            card_1.style.backgroundPosition = 'center';
+            card_2.style.backgroundPosition = 'center';
             card_1.innerHTML = '';
-            card_2.style.backgroundColor = 'lightblue'; //'url(tile_bg.jpg) no-repeat';
             card_2.innerHTML = '';
             values = [];
             ids = [];
-          }
+          }//http://imgur.com/lA0KNNt
         }
       }
       setTimeout(flip, 100);
     }
   }
 }
+newBoard();
+//   })
+//   .fail(function(err) {
+//     console.log(err);
+//   });
+// });
