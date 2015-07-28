458a387c913483929d7d71decdd185d5aba46a91
Stuart Hiller <hiller.stuart@gmail.com>
2015-07-28 11:21:54 -0700

    Able to create users and log in to user array

--- public/log-in.js
+++ public/log-in.js
@@ -5,7 +5,7 @@ var Users = function(userName, passWord, score) {
 };
 
 var userAry = [];
-
+var logInStatus = false
 var createNewUser = document.getElementById('user-input');
 createNewUser.addEventListener('submit', function(e) {
   e.preventDefault();
@@ -16,7 +16,6 @@ createNewUser.addEventListener('submit', function(e) {
   } else {
       if (!(checkForDoubles(userName, userAry))) {
       userAry.push(new Users(userName, passWord, 0));
-      console.dir(userName);
       console.dir(userAry);
     }
   }
@@ -27,7 +26,35 @@ function checkForDoubles(userName, userAry) {
     if (userName == userAry[i].userName) return true;
   }
   return false;
-}
+};
+
+function checkForPassword(passWord, userAry) {
+  for (var i = 0; i < userAry.length; i++) {
+    if (passWord == userAry[i].passWord) return true;
+  }
+  return false;
+};
+
+var logIn = document.getElementById('user-input-login');
+logIn.addEventListener('submit', function(e) {
+  e.preventDefault();
+  var userName = logIn.elements[0].value;
+  var passWord = logIn.elements[1].value;
+  if (!(userName && passWord)) {
+    return;
+  } else {
+    if(checkForDoubles(userName, userAry)){
+      if(checkForPassword(passWord, userAry)) {
+      logInStatus = true;
+      console.log(logInStatus);
+    } else {
+      alert("Wrong PassWord or User name");
+    }
+    } else {
+      alert("Wrong PassWord or User name");
+    }
+  }
+});
 
 
 
