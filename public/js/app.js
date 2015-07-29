f0d33105559f62c5f67d63f588fbaac8d6cfe619
JEILYMAR BRADY <jeilymarb@gmail.com>
2015-07-29 00:06:29 -0700

    cleaned it up a bit

--- public/js/app.js
+++ public/js/app.js
@@ -9,101 +9,86 @@ $(document).ready(function() {
   })
   .done(function(res) {
     pics = res.data.images;
-    pullFromImgur(pics);
-})
+    var Picture = function(path){
+      this.flipped = false;
+      this.img = document.createElement('img');
+      this.img.visited = false;
+      this.img.logo = 'logo.png';
+      this.img.path = path;
+      this.img.src = this.img.logo;
+      this.img.addEventListener('click', function(e) {
+        e.preventDefault();
+        gameTime(e.target);
+      }, true);
+      this.render = function(){
+        var container = document.getElementById('board');
+        container.appendChild(this.img);
+      };
+    };
+
+    Array.prototype.shuffle = function() {
+      var i = this.length, j, temp;
+      while(--i> 0){
+        j = Math.floor(Math.random() * (i+1));
+        temp = this[j];
+        this[j] = this[i];
+        this[i] = temp;
+      }
+    };
+
+    //Global variables
+    var cards_array;
+    var values = [];
+    var flipped = 0;
+    var clicks = 0;
+
+    function pullFromImgur() {
+      cards_array = [];
+      for(var i = 0; i < 2; i++){
+        for(var j = 0; j < pics.length; j++){
+          cards_array.push(new Picture(pics[j].link));
+        }
+      }
+      cards_array.shuffle();
+      for (var k = 0; k < cards_array.length; k++) {
+        cards_array[k].render();
+      }
+    }
+    pullFromImgur();
+
+    function gameTime(e){
+      var pic = e;
+      if(!(pic.visited) && values.length < 2){
+        pic.visited = true;
+        pic.src = pic.path;
+      }
+        values.push(pic);
+        clicks++;
+        if(values.length===2){
+          if(values[0].path===values[1].path){
+            flipped+=2;
+            values = [];
+            if(flipped === cards_array.length){
+              alert("A new game has been started! You used " + clicks + " clicks to find all of the pairs.");
+              document.getElementById('board').innerHTML = '';
+              pullFromImgur();
+            }
+          } else {
+              setTimeout(flip, 700);
+            }
+        }
+
+    }
+
+    function flip(){
+      for (var i = 0; i < values.length; i++) {
+        values[i].src = values[i].logo;
+        values[i].visited = false;
+      }
+      values = [];
+    }
+  })
   .fail(function(err) {
     console.log(err);
   });
 });
-
-var Picture = function(path){
-  this.flipped = false;
-  this.img = document.createElement('img');
-  this.img.visited = false;
-  this.img.logo = 'logo.png';
-  this.img.path = path;
-  this.img.src = this.img.logo;
-  this.img.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.dir(e.target);
-    gameTime(e.target);
-  }, true);
-  this.render = function(){
-    var container = document.getElementById('board');
-    container.appendChild(this.img);
-  };
-};
-
-Array.prototype.shuffle = function() {
-  var i = this.length, j, temp;
-  while(--i> 0){
-    j = Math.floor(Math.random() * (i+1));
-    temp = this[j];
-    this[j] = this[i];
-    this[i] = temp;
-  }
-};
-
-var cards_array;
-function pullFromImgur(picsArray) {
-  var pics = picsArray;
-  cards_array = [];
-  for(var i = 0; i < 2; i++){
-    for(var j = 0; j < pics.length; j++){
-      cards_array.push(new Picture(pics[j].link));
-    }
-  }
-  cards_array.shuffle();
-  for (var k = 0; k < cards_array.length; k++) {
-    cards_array[k].render();
-  }
-  console.dir(cards_array);
-}
-
-//Global variables
-var values = [];
-var ids = [];
-var flipped = 0;
-var clicks = 0;
-
-function gameTime(e){
-  var pic = e;
-  if(!(pic.visited) && values.length < 2){
-    pic.visited = true;
-    pic.src = pic.path;
-    console.log(pic.visited);
-    if(values.length===0){
-      values.push(pic);
-      ids.push(pic);
-      clicks++;
-    } else if (values.length === 1){
-      values.push(pic);
-      ids.push(pic);
-      clicks++;
-      if(values[0].path===values[1].path){
-        console.log('match');
-        flipped+=2;
-        values = [];
-        ids = [];
-        if(flipped === cards_array.length){
-          alert("A new game has been started! You used " + clicks + " clicks to find all of the pairs.");
-          document.getElementById('board').innerHTML = '';
-          pullFromImgur();
-        }
-      } else {
-        setTimeout(flip, 700);
-      }
-    }
-  }
-}
-
-function flip(){
-  console.log('flipBack');
-  console.log(values[0]);
-  values[0].src = values[0].logo;
-  values[1].src = values[1].logo;
-  values[0].visited = false;
-  values[1].visited = false;
-  values = [];
-  ids = [];
-}
