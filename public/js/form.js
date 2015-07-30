9a050b80429ae11c94832d038ff129e39cf86d9a
Emily Landi <emily.landi@gmail.com>
2015-07-29 18:10:30 -0700

    added user login to index.html, fixed formating

--- public/js/form.js
+++ public/js/form.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
 		$('#sub').fadeOut('slow');
 		$('#user-input').attr('id', 'user-input-login');
 		$('#formTitle').fadeOut('slow', function() {
-			$(this).replaceWith('<p id="formTitle">Register</p>');
+		$(this).replaceWith('<p id="formTitle">Register</p>');
 		});
 	});
 	$('#login').click(function(evt) {
 		evt.preventDefault();
 		console.log('Collect user information.')
+		$('#register').replaceWith('<button id="submit">Login</button>');
 	});
-	$('#register').replaceWith('<button id="submit">Login</button>');
 });
